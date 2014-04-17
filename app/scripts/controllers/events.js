3291ea5e73967a7a486dd2921710109d742dd02b
kidaikwon <kidai_kwon@brown.edu>
2014-04-17 17:59:22 -0400

    added google maps. need to fix displaying location in view.html

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -6,43 +6,44 @@ angular.module('11thhourApp')
     $scope.showFilters = false;
 
     $scope.categories = ['food', 'rideshare', 'haha'];
-    $scope.selectionCategories = [];
+    $scope.selectedCategories = [];
 
     $scope.prices = ['$', '$$', '$$$', '$$$$'];
-    $scope.selectionPrice = undefined;
-    
+    $scope.selectedPrice = undefined;
+
+    $scope.selectedLocation = undefined;    
 
     $scope.toggleCategorySelection = function toggleCategorySelection(category, type) {
-      var idx = $scope.selectionCategories.indexOf(category);
+      var idx = $scope.selectedCategories.indexOf(category);
       // is currently selected
       if (idx > -1) {
-        $scope.selectionCategories.splice(idx, 1);
+        $scope.selectedCategories.splice(idx, 1);
       }
       // is newly selected
       else {
-        $scope.selectionCategories.push(category);
+        $scope.selectedCategories.push(category);
       }
       if (type === 'filter') {
         $scope.filters();
       }
     };
     $scope.isCategoryActive = function(category) {
-      var idx = $scope.selectionCategories.indexOf(category);
+      var idx = $scope.selectedCategories.indexOf(category);
       return (idx > -1);
     };
 
 
     $scope.togglePriceSelection = function togglePriceSelection(price) {
-      if ($scope.selectionPrice !== price) {
-        $scope.selectionPrice = price;
+      if ($scope.selectedPrice !== price) {
+        $scope.selectedPrice = price;
       }
     };
     $scope.isPriceActive = function(price) {
-      return ($scope.selectionPrice === price);
+      return ($scope.selectedPrice === price);
     };
     $scope.initSelectedPrice = function(price) {
       if (price === $scope.prices[0]) {
-        $scope.selectionPrice = price;
+        $scope.selectedPrice = price;
       }
     }
 
@@ -55,11 +56,11 @@ angular.module('11thhourApp')
     };
 
     $scope.filters = function() {
-      if (this.selectionCategories.length == 0) {
+      if (this.selectedCategories.length == 0) {
         $location.search({}).path('/');
       }
       else {
-        $location.search({ categories: this.selectionCategories.join() }).path('search');
+        $location.search({ categories: this.selectedCategories.join() }).path('search');
       }
     };
 
@@ -68,11 +69,11 @@ angular.module('11thhourApp')
         title: this.title,
         description: this.description,
         time: this.time,
-        location: this.location,
-        price: $scope.selectionPrice.length,
+        location: JSON.stringify($scope.selectedLocation),
+        price: $scope.selectedPrice.length,
         peopleNeeded: this.peopleNeeded,
         attendees: this.attendees,
-        categories: this.selectionCategories
+        categories: this.selectedCategories
       });
       event.$save(function(response) {
         $location.path("events/" + response._id);
@@ -140,7 +141,7 @@ angular.module('11thhourApp')
       }
       categories = categories.split(',');
       for (var i in categories) {
-        $scope.selectionCategories.push(categories[i]);
+        $scope.selectedCategories.push(categories[i]);
       }
     }
 
@@ -149,6 +150,7 @@ angular.module('11thhourApp')
         eventId: $routeParams.eventId
       }, function(event) {
         $scope.event = event;
+        displayViewLocationMap(event.location);
         
         if ($rootScope.currentUser !== null) {
           var index = event.attendees.indexOf($rootScope.currentUser.username);
@@ -173,4 +175,139 @@ angular.module('11thhourApp')
         }
       );
     };
+
+
+
+
+    // =============
+    $scope.initializeCreateLocationMap = function() {
+      var mapOptions = {
+        center: new google.maps.LatLng(41.826718, -71.40256199999999),
+        zoom: 13,
+        disableDefaultUI: true
+      };
+      var map = new google.maps.Map(document.getElementById('createLocationMap'),
+        mapOptions);
+
+      var input = /** @type {HTMLInputElement} */(
+          document.getElementById('createLocationInput'));
+
+      var autocomplete = new google.maps.places.Autocomplete(input);
+      autocomplete.bindTo('bounds', map);
+
+      var infowindow = new google.maps.InfoWindow();
+      var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+      });
+
+      google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+          return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+          map.fitBounds(place.geometry.viewport);
+        } else {
+          map.setCenter(place.geometry.location);
+          map.setZoom(15);
+        }
+
+        // @type {google.maps.Icon}
+        marker.setIcon(({
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(35, 35)
+        }));
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        var address = '';
+        if (place.address_components) {
+          address = [
+            (place.address_components[0] && place.address_components[0].short_name || ''),
+            (place.address_components[1] && place.address_components[1].short_name || ''),
+            (place.address_components[2] && place.address_components[2].short_name || '')
+          ].join(' ');
+        }
+
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+        infowindow.open(map, marker);
+        $scope.selectedLocation = place;
+      });
+    };
+
+    function displayViewLocationMap(location) {
+      if (location === undefined || location === '') {
+        return;
+      }
+      
+      var place = JSON.parse(location);
+      if (!place.geometry) {
+        return;
+      }
+
+      var mapOptions = {
+        center: new google.maps.LatLng(41.826718, -71.40256199999999),
+        zoom: 13,
+        disableDefaultUI: true
+      };
+      var map = new google.maps.Map(document.getElementById('viewLocationMap'),
+        mapOptions);
+
+      var infowindow = new google.maps.InfoWindow();
+      var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+      });
+
+      // infowindow.close();
+      marker.setVisible(false);
+      
+      /*
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        // console.log(place.geometry.location);
+        // place.geometry.location.k = parseFloat(place.geometry.location.k);
+        // place.geometry.location.A = parseFloat(place.geometry.location.A);
+        console.log(place.geometry.location);
+        map.setCenter(place.geometry.location);
+        map.setZoom(15);
+      }
+
+      // @type {google.maps.Icon}
+      marker.setIcon(({
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(35, 35)
+      }));
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+
+      var address = '';
+      if (place.address_components) {
+        address = [
+          (place.address_components[0] && place.address_components[0].short_name || ''),
+          (place.address_components[1] && place.address_components[1].short_name || ''),
+          (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+      }
+
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+      infowindow.open(map, marker);
+      */
+    };
+
+
+
   });
